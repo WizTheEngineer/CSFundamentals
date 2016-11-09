280c0290a1c0190e1ff2b6be9be6895cc87a5b05
Wayne B. Jackson <wayne.b.jackson@gmail.com>
2016-11-08 22:15:21 -0600

    Fixed Trie Implementation

--- src/data_structures/Trie.java
+++ src/data_structures/Trie.java
@@ -10,72 +10,109 @@ import java.util.HashMap;
 /**
  *
  * @author Wayne
- * Implementation of a Trie that uses a {@link HashMap}. This data structure
- * is useful for input validation type problems.
+ * Implementation of a Trie that does not support duplicates.
  */
 public class Trie {
-    private TrieNode root;
+    private final TrieNode root = new TrieNode(false);
     
-    public Trie() {
-        root = new TrieNode('*');
-    }
+    public Trie() {}
     
     public void addWord(String word) {
-        TrieNode node = root;
-        
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            node.addChild(c);
-            node = node.getChild(c);
+        // Make sure the word is not empty
+        if (word.isEmpty()) {
+            return;
         }
         
-        if (!node.isWord()) {
-            // Set the value of is word on the last to true
-            node.setIsWord(true);
+        // Make sure the word is not in this Trie
+        if (containsWord(word)) {
+            return;
+        }
+        
+        // Start at the root
+        TrieNode node = root;
+        
+        // Loop through and add the word to the Trie
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            boolean endOfWord = i == (word.length() - 1);
+            node.addChild(c, endOfWord);
+            node = node.getChild(c);
         }
     }
     
     public boolean containsWord(String word) {
+        if (word.isEmpty()) {
+            return false;
+        }
+        
+        // Start at the root
         TrieNode node = root;
         
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            
             if (!node.hasChild(c)) {
                 return false;
             }
-            
-            // Switch to the next node
             node = node.getChild(c);
         }
-        return true;
+        
+        return node.isEndOfWord();
+    }
+    
+    /**
+     * Returns the number of words contained in the Trie that start with a 
+     * given prefix.
+     * @param prefix
+     * @return 
+     */
+    public int partialLookup(String prefix) {
+        if (prefix.isEmpty()) {
+            return 0;
+        }
+        
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            
+            if (!node.hasChild(c)) {
+                return 0;
+            }
+            
+            node = node.getChild(c);
+        }
+        
+        // If the node is an end of a word count it towards the total count
+        if (node.isEndOfWord()) {
+            return node.getChildCount() + 1;
+        }
+        return node.getChildCount();
     }
     
     private class TrieNode {
-        private char value;
-        private HashMap<Character, TrieNode> children;
+        private final HashMap<Character, TrieNode> children;
+        private boolean endOfWord;
         private int childCount;
-        private boolean isWord;
         
-        public TrieNode(char value) {
-            this.value = value;
+        public TrieNode(boolean endOfWord) {
+            this.endOfWord = endOfWord;
             children = new HashMap();
             childCount = 0;
-            isWord = false;
         }
         
-        public char getValue() {
-            return value;
-        }
-        
-        public void addChild(char c) {
-            if (children.get(c) == null) {
-                TrieNode node = new TrieNode(c);
+        public void addChild(char c, boolean endOfWord) {
+            TrieNode node;
+            if (!children.containsKey(c)) {
+                node = new TrieNode(endOfWord);
                 children.put(c, node);
-                childCount++;
             } else {
-                System.out.println("Node for character " + value + " contains a node for character " + c);
+                node = children.get(c);
+                
+                if (endOfWord) {
+                    node.setIsEndOfWord(true);
+                }
             }
+            
+            childCount++;
         }
         
         public boolean hasChild(char c) {
@@ -86,17 +123,16 @@ public class Trie {
             return children.get(c);
         }
         
+        public boolean isEndOfWord() {
+            return endOfWord;
+        }
+        
+        public void setIsEndOfWord(boolean endOfWord) {
+            this.endOfWord = endOfWord;
+        }
+        
         public int getChildCount() {
             return childCount;
-        }
-        
-        public boolean isWord() {
-            return isWord;
-        }
-        
-        public void setIsWord(boolean isWord) {
-            this.isWord = isWord;
-            System.out.println("Setting the node with char " + value + " is word to " + isWord);
         }
     }
 }
