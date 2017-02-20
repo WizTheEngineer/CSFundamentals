43e8674d81c9be1f6b452433f558bee7cb287fa9
Wayne Jackson <wayne.jackson@bottlerocketstudios.com>
2017-02-20 15:19:25 -0600

    Added solution for building tree from inorder and preorder traversals.

--- src/csfundamentals/CSFundamentals.java
+++ src/csfundamentals/CSFundamentals.java
@@ -6,14 +6,7 @@
 package csfundamentals;
 
 import ctcisolutions.TreesAndGraphs;
-import data_structures.AVLTree;
-import data_structures.BST;
-import data_structures.BST.BinaryNode;
-import data_structures.Graph;
-import data_structures.Graph.GraphNode;
-import data_structures.LinkedList;
-import data_structures.Node;
-import java.util.List;
+import ctcisolutions.TreesAndGraphs.SpecialNode;
 
 /**
  *
@@ -25,47 +18,9 @@ public class CSFundamentals {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GraphNode<Integer> three = new GraphNode(3);
-        GraphNode<Integer> four = new GraphNode(4);
-        GraphNode<Integer> five = new GraphNode(5);
-        GraphNode<Integer> six = new GraphNode(6);
-        GraphNode<Integer> seven = new GraphNode(7);
-        
-        three.addConnection(four);
-        four.addConnection(seven);
-        four.addConnection(three);
-        five.addConnection(four);
-        five.addConnection(seven);
-        seven.addConnection(four);
-        seven.addConnection(six);
-        
-        Graph graph = new Graph(5);
-        graph.addNode(three);
-        graph.addNode(four);
-        graph.addNode(five);
-        graph.addNode(six);
-        graph.addNode(seven);
-        
-        boolean hasPath = TreesAndGraphs.hasRouteBetweenNodes(six, five);
-        System.out.printf("Has path? %b\n", hasPath);
-        
-        int[] sortedArray = {9, 10, 11, 12, 13, 14, 20, 25, 40, 50, 60, 70};
-        BST<Integer> minTree = TreesAndGraphs.buildMinimalTree(sortedArray);
-        minTree.printPretty();
-        
-        List<LinkedList<BinaryNode>> listOfDepths = TreesAndGraphs.listOfDepths(minTree);
-        
-        int level = 1;
-        for (LinkedList<BinaryNode> list : listOfDepths) {
-            System.out.println("List of nodes at level " + level);
-            
-            Node<BinaryNode> start = list.getHead();
-            while (start != null) {
-                System.out.print(" " + start.data.getText() + " ");
-                start = start.next;
-            }
-            level++;
-            System.out.println();
-        }
+        int[] inorder = {10, 30, 40, 50, 60, 70, 80};
+        int[] preorder = {50, 30, 10, 40, 70, 60, 90};
+        SpecialNode root = TreesAndGraphs.buildTree(preorder, inorder);
+        root.printInorder();
     }
 }
--- src/ctcisolutions/TreesAndGraphs.java
+++ src/ctcisolutions/TreesAndGraphs.java
@@ -21,17 +21,17 @@ import java.util.ArrayList;
  * @author Wayne
  */
 public class TreesAndGraphs {
-    
+
     // #1 Has Route Between Nodes
     public static boolean hasRouteBetweenNodes(GraphNode one, GraphNode two) {
         return bfsForPath(one, two) || bfsForPath(two, one);
     }
-    
+
     private static boolean bfsForPath(GraphNode root, GraphNode destination) {
         Queue<GraphNode> queue = new Queue();
         HashSet<GraphNode> visited = new HashSet();
         queue.enqueue(root);
-        
+
         while (!queue.isEmpty()) {
             GraphNode node = queue.dequeue(); // Remove from the front of the queue
             if (node == destination) return true;
@@ -47,38 +47,38 @@ public class TreesAndGraphs {
         }
         return false;
     }
-    
+
     // #2 Binary Search Tree Minimal Height Sorted Array
     public static BST<Integer> buildMinimalTree(int[] sortedArray) {
         BST<Integer> tree = new BST();
         buildMinimalTree(tree, sortedArray, 0, sortedArray.length - 1);
         return tree;
     }
-    
+
     private static void buildMinimalTree(BST<Integer> tree, int[] sortedArray, int start, int end) {
         if (start > end) {
             return;
         }
-        
+
         // Find the middle element and insert it into the tree
         int middleIndex = (start + end) / 2;
         tree.insert(sortedArray[middleIndex]);
         buildMinimalTree(tree, sortedArray, start, middleIndex - 1);
         buildMinimalTree(tree, sortedArray, middleIndex + 1, end);
     }
-    
+
     // #3 List of Depths
     public static List<LinkedList<BinaryNode>> listOfDepths(BST tree) {
         if (tree.isEmpty()) throw new IllegalArgumentException("Tree cannot be empty");
-        
+
         // Maps
         HashMap<Integer, List<BinaryNode>> levelMap = new HashMap();
-        
+
         // Build the level map
         buildLevelMap(tree.getRoot(), levelMap, 1);
-        
+
         List<LinkedList<BinaryNode>> listOfDepths = new ArrayList();
-        
+
         for (int key : levelMap.keySet()) {
             LinkedList<BinaryNode> depthList = new LinkedList();
             List<BinaryNode> elements = levelMap.get(key);
@@ -87,7 +87,7 @@ public class TreesAndGraphs {
             }
             listOfDepths.add(depthList);
         }
-        
+
         return listOfDepths;
     }
 
@@ -100,5 +100,53 @@ public class TreesAndGraphs {
         levelMap.get(level).add(root);
         buildLevelMap(root.left, levelMap, level+1);
         buildLevelMap(root.right, levelMap, level+1);
+    }
+
+    // Bonus: Reconstruct binary tree from preorder and inorder traversals
+    public static SpecialNode buildTree(int[] preorder, int[] inorder) {
+        return construct(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    private static SpecialNode construct(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd){
+        if(preStart > preEnd || inStart > inEnd){
+            return null;
+        }
+        
+        // The root is the first node in the preorder
+        SpecialNode root = new SpecialNode(preorder[preStart]);
+        int rootValue = root.data;
+        
+        // Find the index of the root in the inorder traversal
+        int rootInorderPos = inStart;
+        for (int i = inStart; i <= inEnd; i++) {
+            int value = inorder[i];
+            if (value == rootValue) {
+                rootInorderPos = i;
+            }
+        }
+        
+        root.left = construct(preorder, preStart + 1, preStart + rootInorderPos, inorder, inStart, rootInorderPos - 1);
+        root.right = construct(preorder, preStart + rootInorderPos + 1, preEnd, inorder, rootInorderPos + 1, inEnd);
+        return root;
+    }
+
+    public static class SpecialNode {
+        public int data;
+        public SpecialNode left, right;
+
+        public SpecialNode(int data) {
+            this.data = data;
+        }
+
+        public void printInorder() {
+            printInorder(this);
+        }
+
+        private void printInorder(SpecialNode node) {
+            if (node == null) return;
+            printInorder(node.left);
+            System.out.printf("%d ", node.data);
+            printInorder(node.right);
+        }
     }
 }
